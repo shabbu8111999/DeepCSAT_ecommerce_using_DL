7dc7613c5836019b5cdeaeb4b7c5c9c984ad0f64
Shabareesh Nair <shabareesh08@gmail.com>
2026-03-15 13:49:36 +0530

    Script file updated

--- static/script.js
+++ static/script.js
@@ -1,71 +1,73 @@
 async function predictCSAT(){
 
-const remark = document.getElementById("remark").value.trim();
-const response_time = document.getElementById("response_time").value;
-const survey_delay = document.getElementById("survey_delay").value;
-const issue_hour = document.getElementById("issue_hour").value;
-const issue_day = document.getElementById("issue_day").value;
-const issue_month = document.getElementById("issue_month").value;
+// get input values
+const remark = document.getElementById("remark").value
 
-const resultDiv = document.getElementById("result");
-const loader = document.getElementById("loader");
+const response_time = document.getElementById("response_time").value
 
-resultDiv.innerHTML = "";
+const survey_delay = document.getElementById("survey_delay").value
 
-if(remark === ""){
+const issue_hour = document.getElementById("issue_hour").value
 
-resultDiv.innerHTML = "Please enter customer remark";
-return;
+const issue_day = document.getElementById("issue_day").value
 
-}
+const issue_month = document.getElementById("issue_month").value
+
 
 const payload = {
 
 remark: remark,
+
 response_time: response_time,
+
 survey_delay: survey_delay,
+
 issue_hour: issue_hour,
+
 issue_day: issue_day,
+
 issue_month: issue_month
 
-};
+}
 
-loader.classList.remove("hidden");
-
-try{
 
 const response = await fetch("/predict",{
 
 method:"POST",
+
 headers:{
+
 "Content-Type":"application/json"
+
 },
 
 body:JSON.stringify(payload)
 
-});
+})
 
-const data = await response.json();
 
-loader.classList.add("hidden");
+const data = await response.json()
 
-if(data.error){
 
-resultDiv.innerHTML = data.error;
-return;
+// show predicted CSAT
+document.getElementById("result").innerHTML =
+
+"Predicted CSAT Score : " + data.predicted_csat
+
+
+// show probabilities
+let prob_html = "<h3>Confidence</h3>"
+
+
+for(let i=0;i<data.probabilities.length;i++){
+
+let percent = (data.probabilities[i]*100).toFixed(2)
+
+prob_html += "CSAT " + (i+1) + " : " + percent + "%<br>"
 
 }
 
-resultDiv.innerHTML = "Predicted CSAT Score : " + data.predicted_csat;
 
-}
-
-catch(error){
-
-loader.classList.add("hidden");
-
-resultDiv.innerHTML = "Prediction failed";
-
-}
+document.getElementById("probabilities").innerHTML = prob_html
 
 }
